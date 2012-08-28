5ddc9e7fb7d6e8037343b42baf311f297481c027
Jonathan Claridge <claridge@gmail.com>
2012-08-27 20:58:42 -0700

    Script for running accuracy tests.  Works for 1d due to changes to */accuracy_test.py from previous commit.

---
+++ run_tests.py
@@ -0,0 +1,34 @@
+# This is a terribly hacky way of running tests.
+
+import os
+import subprocess
+
+TEST_FILES = [
+  'applications/1d/PorousMedium/accuracy_test.py',
+  'applications/1d/ThinFilm/accuracy_test.py',
+  'applications/1d/LinearDiffusion/accuracy_test.py',
+  'applications/1d/LinearFourthOrder/accuracy_test.py'
+  ]
+
+HOME = os.getcwd()  
+
+for test_file in TEST_FILES:
+  os.chdir(HOME)
+  directory = os.path.dirname(test_file)
+  file_name = os.path.basename(test_file)
+  os.chdir(directory)
+  
+  return_code = subprocess.call(['make', 'xclaw'])
+  if return_code:
+    print 'Make failed'
+  else:
+    subprocess.call(['python', file_name])
+  raw_input('Press ENTER to continue')
+  
+
+for test_file in TEST_FILES:
+  os.chdir(HOME)
+  directory = os.path.dirname(test_file)
+  os.chdir(directory)
+  return_code = subprocess.call(['make', 'clobber'])
+  
