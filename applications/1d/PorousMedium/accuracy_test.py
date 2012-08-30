39abef7fb464675eea8545effa6d0048b48a6845
Jonathan Claridge <claridge@gmail.com>
2012-08-29 18:55:10 -0700

    Adding library routine for accuracy testing.  Converted 1d/PorousMedium to use it.

--- applications/1d/PorousMedium/accuracy_test.py
+++ applications/1d/PorousMedium/accuracy_test.py
@@ -1,50 +1,21 @@
-import setrun
-import setplot
-import pyclaw.data
-import pyclaw.runclaw
-from pyclaw.plotters import plotclaw
-
 from numpy import *
-from numpy import linalg
+import accuracy_testing
 import pylab
-
-import claw_solution_1d
-import unittest
-
-_MASS = 1.
-_T0 = .3
+import pyclaw.data
 
 
-def TrueSolution(x, t):
-    tau = t + _T0
-    tmp = (_MASS - 1./12. * x**2 / tau**(2./3.)) / tau**(1./3.)
-    return tmp if tmp > 0. else 0.
-
-
-def BuildDefaultClawRunData():
+def BuildRunData():
   rundata = pyclaw.data.ClawRunData(pkg='Classic', ndim=1)
 
   probdata = rundata.new_UserData(name='probdata', fname='setprob.data')
   probdata.add_param('implicit_integration_scheme', 'Crank-Nicolson')
-  probdata.add_param('max_time_step_splits', 0,
-                     'Max number of times to halve the lenght of the '
-                     'implicit time step, should Newton''s method fail to '
-                     'converge.')
-  probdata.add_param('newton_max_iter', 30,
-                     'Max iterations for Newton''s method before enforcing '
-                     'reduction criterion')
-  probdata.add_param('newton_reduction_factor', .5,
-                     'Required reduction in residual norm for Newton''s method '
-                     'to continue beyond newton_max_iter iterations')
-  probdata.add_param('newton_tolerance', 1e-8, 'Newton''s method stops when '
-                     'norm(delta(iterate)) is below this.')
-  probdata.add_param('newton_verbosity', 0, 'Logging level for Newton''s method')
-
-  probdata.add_param('cg_tolerance', 1e-8, 'Conjugate gradient stops when '
-                     'norm(residual) is below this.')
-  probdata.add_param('cg_verbosity', 0, 'Logging level for CG/BiCGStab')
-
-
+  probdata.add_param('max_time_step_splits', 0)
+  probdata.add_param('newton_max_iter', 10)
+  probdata.add_param('newton_reduction_factor', .5)
+  probdata.add_param('newton_tolerance', 1e-8)
+  probdata.add_param('newton_verbosity', 0)
+  probdata.add_param('cg_tolerance', 1e-8)
+  probdata.add_param('cg_verbosity', 0)
 
   clawdata = rundata.clawdata
   clawdata.ndim = 1
@@ -57,7 +28,6 @@ def BuildDefaultClawRunData():
   clawdata.nout = 1
   clawdata.verbosity = 0
   clawdata.dt_variable = 0
-  clawdata.dt_initial = 1e-1
   clawdata.dt_max = 1e+99
   clawdata.cfl_desired = 0.9
   clawdata.cfl_max = 1.0
@@ -72,104 +42,55 @@ def BuildDefaultClawRunData():
   clawdata.mthbc_xupper = 0
   clawdata.t0 = 0.0  # Setting this in clawdata doesn't appear to work.
 
-
-  # Specify these manually
+  # These will be specified separately for each test.
   clawdata.mx = None
   clawdata.tfinal = None
+  clawdata.dt_initial = None
 
   return rundata
 
-    
-class RefinementTest(object):
 
-  def __init__(self, t_final, dt_values, min_mx):
-    self._t_final = t_final
-    self._dt_values = dt_values
-
-    max_dt = max(dt_values)
-    mx_values = [int(round(min_mx * max_dt / dt)) for dt in dt_values]
-    self._mx_values = array(mx_values)  
-
-  def RunSimulations(self):
-    rundata = BuildDefaultClawRunData()
-    clawdata = rundata.clawdata
-    clawdata.tfinal = self._t_final
-    
-    for i in xrange(len(dt_values)):
-      clawdata.dt_initial = self._dt_values[i]
-      clawdata.mx = self._mx_values[i]
-      rundata.write()
-      pyclaw.runclaw.runclaw(xclawcmd='xclaw > /dev/null', outdir='_output%02d' % i)
-
-  def GetNumericalErrors(self):
-    numerical_errors = {'l1': [], 'l2': [], 'linf': []}
-    for i in xrange(len(self._dt_values)):
-      solution = claw_solution_1d.ClawSolution('_output%02d' % i)
-      solution.SetFrame(1)
-      true_solution = array([TrueSolution(x, self._t_final)
-                             for x in solution.GetCellCenters()])
-      cellwise_error = abs(solution.q[:,0] - true_solution)
-
-      numerical_errors['l1'].append(sum(cellwise_error) * solution.dx)
-      numerical_errors['l2'].append(sqrt(sum(cellwise_error**2)) * solution.dx)
-      numerical_errors['linf'].append(max(cellwise_error))
-
-    return numerical_errors
+_MASS = 1.
+_T0 = .3
 
 
-def PowerFit(error_values, dt_values):
-  """Fits error values with a curve c*dt**exponent.
-  
-  Args:
-    error_values: Vector of error values.
-    dt_values: Vector of dt values, parallel to error_values.
-  """
-  A = vstack([ones(len(error_values)), log(dt_values)]).T
-  c, exponent = linalg.lstsq(A, log(error_values))[0]
-  return exp(c), exponent
+def TrueSolution(x, t):
+    tau = t + _T0
+    tmp = (_MASS - 1./12. * x**2 / tau**(2./3.)) / tau**(1./3.)
+    return tmp if tmp > 0. else 0.
 
 
-t_final = 0.5
-num_steps = [round(x) for x in logspace(log10(5), log10(50), 11)]
-dt_values = array([t_final / n for n in num_steps])
-
-refinement_test = RefinementTest(t_final, dt_values, 10)
-refinement_test.RunSimulations()
-
-numerical_errors = refinement_test.GetNumericalErrors()
-
-l1_factor, l1_exponent = PowerFit(numerical_errors['l1'], dt_values)
-l2_factor, l2_exponent = PowerFit(numerical_errors['l2'], dt_values)
-linf_factor, linf_exponent = PowerFit(numerical_errors['linf'], dt_values)
-
-
-def GetTestMessages():
-  def _OkOrFail(passing):
-    return 'ok' if passing else 'FAILURE'
-
-  messages = []
-  passing = l1_exponent > 1.9
-  messages.append('L1 error ~ dt**%f ... %s' % 
-                  (l1_exponent, _OkOrFail(passing)))
-
-  passing = l2_exponent > 1.9
-  messages.append('L2 error ~ dt**%f ... %s' % 
-                  (l2_exponent, _OkOrFail(passing)))
-
-  passing = linf_exponent > 1.9
-  messages.append('Linf error ~ dt**%f ... %s' % 
-                  (linf_exponent, _OkOrFail(passing)))
-  return messages
-    
 if __name__ == '__main__':
-  print '\n'.join(GetTestMessages())
-# pylab.loglog(dt_values, numerical_errors['l1'], 'ro', label='$L^1$ error')
-# pylab.loglog(dt_values, l1_factor*dt_values**l1_exponent, 'r:', label='$\sim \Delta t^{%.3f}$' % l1_exponent)
-# 
-# pylab.loglog(dt_values, numerical_errors['l2'], 'go', label='$L^2$ error')
-# pylab.loglog(dt_values, l2_factor*dt_values**l2_exponent, 'g:', label='$\sim \Delta t^{%.3f}$' % l2_exponent)
-# 
-# pylab.loglog(dt_values, numerical_errors['linf'], 'bo', label='$L^\infty$ error')
-# pylab.loglog(dt_values, linf_factor*dt_values**linf_exponent, 'b:', label='$\sim \Delta t^{%.3f}$' % linf_exponent)
-# 
-# pylab.legend(loc='lower right')
+  run_simulations, show_plots = accuracy_testing.ParseFlags()
+
+  t_final = 0.5
+  steps1 = t_final / 1e-3
+  steps2 = t_final / 1e-4
+  num_steps = [round(x) for x in logspace(log10(steps1), log10(steps2), 11)]
+  dt_values = array([t_final / n for n in num_steps])
+  mx_min = 10
+
+  test = accuracy_testing.AccuracyTest(BuildRunData, TrueSolution, t_final, dt_values, mx_min)
+
+  if run_simulations:
+    test.RunSimulations()
+
+  test.CalculateErrors()
+  
+  for name in sorted(test.errors.keys()):
+    print test.CheckConvergenceOrder(name, 1.9)
+  
+  
+  if show_plots:
+    test.errors['L1'].Plot('ro')
+    test.errors['L1'].PlotDtFit('r:')
+
+    test.errors['L2'].Plot('go')
+    test.errors['L2'].PlotDtFit('g:')
+
+    test.errors['LInfinity'].Plot('bo')
+    test.errors['LInfinity'].PlotDtFit('b:')
+    
+    pylab.legend(loc='lower right')
+
+  raw_input('Press ENTER to finish')
---
+++ lib/python/accuracy_testing.py
@@ -0,0 +1,140 @@
+import setrun
+import setplot
+import pyclaw.data
+import pyclaw.runclaw
+
+from numpy import *
+from numpy import linalg
+import pylab
+import sys
+import getopt
+
+import claw_solution_1d
+
+
+def _GetOutputDirectory(i):
+  return '_output%02d' % i
+
+
+class NumericalError(object):
+  
+  def __init__(self, name):
+    assert name in ('L1', 'L2', 'LInfinity')
+    self.name = name
+    self.dt_values = []
+    self.dx_values = []
+    self.error_values = []
+    self.scale_factor = None
+    self.exponent = None
+  
+  def AddDataPoint(self, dt, dx, cellwise_error):
+    self.dt_values.append(dt)
+    self.dx_values.append(dx)
+    self.error_values.append(self._CalculateErrorValues(dx, cellwise_error))
+  
+  def _CalculateErrorValues(self, dx, cellwise_error):
+    if self.name == 'L1':
+      return sum(abs(cellwise_error)) * dx
+    elif self.name == 'L2':
+      return sqrt(sum(cellwise_error**2)) * dx
+    elif self.name == 'LInfinity':
+      return max(abs(cellwise_error))
+      
+  def PowerFit(self):
+    """Fits error values with a curve c*dt**exponent.
+
+    Args:
+      dt_values: Vector of dt values, parallel to error_values.
+      error_values: Vector of error values.
+    """
+    A = vstack([ones(len(self.error_values)), log(self.dt_values)]).T
+    c, exponent = linalg.lstsq(A, log(self.error_values))[0]
+    self.scale_factor = exp(c)
+    self.exponent = exponent
+
+  # TODO: Pass kwargs?
+  def Plot(self, style='ko'):
+    if self.name == 'L1':
+      legend_name = 'L^1'
+    elif self.name == 'L2':
+      legend_name = 'L^2'
+    elif self.name == 'LInfinity':
+      legend_name = 'L^\infty'
+
+    pylab.loglog(self.dt_values, self.error_values, style,
+                 label='$%s$ error' % legend_name)
+                 
+  def PlotDtFit(self, style='k:'):
+    pylab.loglog(self.dt_values, self.scale_factor*self.dt_values**self.exponent,
+                 style, label='$\sim \Delta t^{%.3f}$' % self.exponent)
+    
+
+class AccuracyTest(object):
+
+  def __init__(self, build_rundata, true_solution,
+               t_final, dt_values, mx_min):
+    self._build_rundata = build_rundata
+    self._true_solution = true_solution
+    self._t_final = t_final
+    self._dt_values = dt_values
+
+    mx_values = [int(round(mx_min * max(dt_values) / dt)) for dt in dt_values]
+    self._mx_values = array(mx_values)
+    
+    self.errors = None
+
+  def RunSimulations(self):
+    rundata = self._build_rundata()
+    clawdata = rundata.clawdata
+    clawdata.tfinal = self._t_final
+
+    for i in xrange(len(self._dt_values)):
+      clawdata.dt_initial = self._dt_values[i]
+      clawdata.mx = self._mx_values[i]
+      rundata.write()
+      pyclaw.runclaw.runclaw(xclawcmd='xclaw > /dev/null',
+                             outdir=_GetOutputDirectory(i))
+
+  def CalculateErrors(self):
+    self.errors = {'L1': NumericalError('L1'),
+                   'L2': NumericalError('L2'),
+                   'LInfinity': NumericalError('LInfinity')}
+    for i in xrange(len(self._dt_values)):
+      solution = claw_solution_1d.ClawSolution(_GetOutputDirectory(i))
+      solution.SetFrame(1)
+      true_solution = array([self._true_solution(x, self._t_final)
+                             for x in solution.GetCellCenters()])
+      cellwise_error = abs(solution.q[:,0] - true_solution)
+      for e in self.errors.itervalues():
+        e.AddDataPoint(self._dt_values[i], solution.dx, cellwise_error)
+
+    for e in self.errors.itervalues():
+      e.PowerFit()
+
+  def CheckConvergenceOrder(self, name, target_order):    
+    error = self.errors[name]
+    exponent = error.exponent
+    passing = exponent > target_order
+    return '%s error ~ dt**%f ... %s' % ( 
+        name, exponent, 'ok' if passing else 'FAILURE')
+
+
+
+def ParseFlags():
+  try:
+    options, arguments = getopt.getopt(sys.argv[1:], '', ['show_plots', 'norun_simulations'])
+  except getopt.GetoptError, err:
+    # print help information and exit:
+    print str(err) # will print something like "option -a not recognized"
+    usage()
+    sys.exit(2)
+
+  run_simulations = True
+  show_plots = False
+  for option, value in options:
+    if option == '--norun_simulations':
+      run_simulations = False
+    elif option == '--show_plots':
+      show_plots = True
+  
+  return run_simulations, show_plots
