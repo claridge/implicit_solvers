41fec2a5ea1c7820808eb6509e6f4de585695a76
Jonathan Claridge <claridge@gmail.com>
2012-09-01 15:35:55 -0700

    Revise stencil calculation script and fill_2_ghost_cells so coefficients are provided externally from their use.

--- lib/1d/fill_2_ghost_cells.f90
+++ lib/1d/fill_2_ghost_cells.f90
@@ -21,21 +21,13 @@ subroutine fill_2_ghost_cells(lower_option, lower_values,  &
     double precision :: x_lower, dx
     common /claw_config/ mx, mbc, x_lower, dx, meqn
 
-    ! TODO: Get rid of old stencils.
-    double precision, dimension(4) :: d0_stencil, d1_stencil, d2_stencil,  &
-        d3_stencil
-    double precision, dimension(4, 0:3) :: stencils
-    common /stencil_config/ d0_stencil, d1_stencil, d2_stencil, d3_stencil, stencils
-
-    ! TODO: Safety check for derivative orders?
-
     character*2, intent(in) :: lower_option, upper_option
     double precision, intent(in), dimension(2) :: lower_values, upper_values
     double precision, intent(inout) :: q(1-mbc:mx+mbc)
 
     integer, dimension(2) :: derivative_orders, ipiv
     integer :: info, i
-    double precision, dimension(2,2) :: a, b
+    double precision, dimension(2,2) ::boundary_coeffs, cell_coeffs
 
     if (lower_option(1:1) == 'p' .or. upper_option(1:1) == 'p') then
         if (lower_option(1:1) /= upper_option(1:1)) then
@@ -49,108 +41,148 @@ subroutine fill_2_ghost_cells(lower_option, lower_values,  &
         return
     end if
 
-    if (lower_option == '01') then
-        q(0) = 8.888888888888888d-01*lower_values(1)  &
-            - 1.333333333333333d+00*dx*lower_values(2)  &
-            + 2.d+00*q(1)  &
-            - 1.111111111111111d-01*q(2)
-        q(-1) = 2.4d+01*lower_values(1)  &
-            - 1.2d+01*dx*lower_values(2)  &
-            + 2.7d+01*q(1)  &
-            - 2.d+00*q(2)
-    else if (lower_option == '02') then
-        q(0) = 2.d+00*lower_values(1)  &
-            + 2.5d-01*dx**2*lower_values(2)  &
-            - 1.d+00*q(1)
-        q(-1) = 2.d+00*lower_values(1)  &
-            + 2.25d+00*dx**2*lower_values(2)  &
-            - 1.d+00*q(2)
-    else if (lower_option == '03') then
-        q(0) = 2.666666666666667d+00*lower_values(1)  &
-            - 1.666666666666667d-01*dx**3*lower_values(2)  &
-            - 2.d+00*q(1)  &
-            + 3.333333333333333d-01*q(2)
-        q(-1) = 8.d+00*lower_values(1)  &
-            - 1.5d+00*dx**3*lower_values(2)  &
-            - 9.d+00*q(1)  &
-            + 2.d+00*q(2)
-    else if (lower_option == '12') then
-        q(0) = 9.230769230769229d-01*dx*lower_values(1)  &
-            + 7.692307692307693d-02*dx**2*lower_values(2)  &
-            + 1.076923076923077d+00*q(1)  &
-            - 7.692307692307693d-02*q(2)
-        q(-1) = 9.230769230769227d-01*dx*lower_values(1)  &
-            + 2.076923076923077d+00*dx**2*lower_values(2)  &
-            + 2.076923076923077d+00*q(1)  &
-            - 1.076923076923077d+00*q(2)
-    else if (lower_option == '13') then
-        q(0) = 1.d+00*dx*lower_values(1)  &
-            - 4.166666666666666d-02*dx**3*lower_values(2)  &
-            + 1.d+00*q(1)
-        q(-1) = 3.d+00*dx*lower_values(1)  &
-            - 1.125d+00*dx**3*lower_values(2)  &
-            + 1.d+00*q(2)
-    else if (lower_option == '23') then
-        q(0) = 1.d+00*dx**2*lower_values(1)  &
-            + 5.d-01*dx**3*lower_values(2)  &
-            + 2.d+00*q(1)  &
-            - 1.d+00*q(2)
-        q(-1) = 3.d+00*dx**2*lower_values(1)  &
-            + 5.d-01*dx**3*lower_values(2)  &
-            + 3.d+00*q(1)  &
-            - 2.d+00*q(2)
-    end if
 
-    if (upper_option == '01') then
-        q(mx+1) = 8.888888888888884d-01*upper_values(1)  &
-            + 1.333333333333333d+00*dx*upper_values(2)  &
-            - 1.111111111111111d-01*q(mx-1)  &
-            + 2.d+00*q(mx)
-        q(mx+2) = 2.4d+01*upper_values(1)  &
-            + 1.2d+01*dx*upper_values(2)  &
-            - 2.d+00*q(mx-1)  &
-            + 2.7d+01*q(mx)
-    else if (upper_option == '02') then
-        q(mx+1) = 2.d+00*upper_values(1)  &
-            + 2.5d-01*dx**2*upper_values(2)  &
-            - 1.d+00*q(mx)
-        q(mx+2) = 2.d+00*upper_values(1)  &
-            + 2.25d+00*dx**2*upper_values(2)  &
-            - 1.d+00*q(mx-1)
-    else if (upper_option == '03') then
-        q(mx+1) = 2.666666666666666d+00*upper_values(1)  &
-            + 1.666666666666666d-01*dx**3*upper_values(2)  &
-            + 3.333333333333333d-01*q(mx-1)  &
-            - 2.d+00*q(mx)
-        q(mx+2) = 7.999999999999998d+00*upper_values(1)  &
-            + 1.5d+00*dx**3*upper_values(2)  &
-            + 2.d+00*q(mx-1)  &
-            - 8.999999999999998d+00*q(mx)
-    else if (upper_option == '12') then
-        q(mx+1) = 9.230769230769229d-01*dx*upper_values(1)  &
-            + 7.692307692307693d-02*dx**2*upper_values(2)  &
-            - 7.692307692307691d-02*q(mx-1)  &
-            + 1.076923076923077d+00*q(mx)
-        q(mx+2) = 9.230769230769229d-01*dx*upper_values(1)  &
-            + 2.076923076923077d+00*dx**2*upper_values(2)  &
-            - 1.076923076923077d+00*q(mx-1)  &
-            + 2.076923076923077d+00*q(mx)
-    else if (upper_option == '13') then
-        q(mx+1) = 1.d+00*dx*upper_values(1)  &
-            + 4.166666666666666d-02*dx**3*upper_values(2)  &
-            + 1.d+00*q(mx)
-        q(mx+2) = 3.d+00*dx*upper_values(1)  &
-            + 1.125d+00*dx**3*upper_values(2)  &
-            + 1.d+00*q(mx-1)
-    else if (upper_option == '23') then
-        q(mx+1) = 9.999999999999998d-01*dx**2*upper_values(1)  &
-            - 4.999999999999998d-01*dx**3*upper_values(2)  &
-            - 9.999999999999998d-01*q(mx-1)  &
-            + 2.d+00*q(mx)
-        q(mx+2) = 3.d+00*dx**2*upper_values(1)  &
-            - 5.d-01*dx**3*upper_values(2)  &
-            - 2.d+00*q(mx-1)  &
-            + 3.d+00*q(mx)
-    end if
+    call get_lower_bc_coefficients(lower_option, dx, boundary_coeffs, cell_coeffs)
+    q(-1) = boundary_coeffs(1, 1) * lower_values(1) + boundary_coeffs(2, 1) * lower_values(2)  &
+        + cell_coeffs(1, 1) * q(1) + cell_coeffs(2, 1) * q(2)
+    q(0) = boundary_coeffs(1, 2) * lower_values(1) + boundary_coeffs(2, 2) * lower_values(2)  &
+        + cell_coeffs(1, 2) * q(1) + cell_coeffs(2, 2) * q(2)
+    
 
+    call get_upper_bc_coefficients(upper_option, dx, boundary_coeffs, cell_coeffs)
+    q(mx+1) = boundary_coeffs(1, 1) * upper_values(1) + boundary_coeffs(2, 1) * upper_values(2)  &
+        + cell_coeffs(1, 1) * q(mx-1) + cell_coeffs(2, 1) * q(mx)
+    q(mx+2) = boundary_coeffs(1, 2) * upper_values(1) + boundary_coeffs(2, 2) * upper_values(2)  &
+        + cell_coeffs(1, 2) * q(mx-1) + cell_coeffs(2, 2) * q(mx)
+    
 end subroutine fill_2_ghost_cells
+
+
+subroutine get_lower_bc_coefficients(orders, dx, boundary_coefficients, cell_coefficients)
+    character*2, intent(in) :: orders
+    double precision, intent(in) :: dx
+    double precision, dimension(2, 2) :: boundary_coefficients
+    double precision, dimension(2, 2) :: cell_coefficients
+    
+    if (orders == '01') then
+        boundary_coefficients(1, 1) = -2.400000000000000d+01
+        boundary_coefficients(2, 1) = -1.200000000000000d+01 * dx
+        cell_coefficients(1, 1) = 2.700000000000000d+01
+        cell_coefficients(2, 1) = -2.000000000000000d+00
+        boundary_coefficients(1, 2) = -8.888888888888888d-01
+        boundary_coefficients(2, 2) = -1.333333333333333d+00 * dx
+        cell_coefficients(1, 2) = 2.000000000000000d+00
+        cell_coefficients(2, 2) = -1.111111111111111d-01
+    else if (orders == '02') then
+        boundary_coefficients(1, 1) = 2.000000000000000d+00
+        boundary_coefficients(2, 1) = 2.250000000000000d+00 * dx**2
+        cell_coefficients(1, 1) = 0.000000000000000d+00
+        cell_coefficients(2, 1) = -1.000000000000000d+00
+        boundary_coefficients(1, 2) = 2.000000000000000d+00
+        boundary_coefficients(2, 2) = 2.500000000000000d-01 * dx**2
+        cell_coefficients(1, 2) = -1.000000000000000d+00
+        cell_coefficients(2, 2) = 0.000000000000000d+00
+    else if (orders == '03') then
+        boundary_coefficients(1, 1) = 8.000000000000000d+00
+        boundary_coefficients(2, 1) = -1.500000000000000d+00 * dx**3
+        cell_coefficients(1, 1) = -9.000000000000000d+00
+        cell_coefficients(2, 1) = 2.000000000000000d+00
+        boundary_coefficients(1, 2) = 2.666666666666667d+00
+        boundary_coefficients(2, 2) = -1.666666666666667d-01 * dx**3
+        cell_coefficients(1, 2) = -2.000000000000000d+00
+        cell_coefficients(2, 2) = 3.333333333333333d-01
+    else if (orders == '12') then
+        boundary_coefficients(1, 1) = -9.230769230769227d-01 * dx
+        boundary_coefficients(2, 1) = 2.076923076923077d+00 * dx**2
+        cell_coefficients(1, 1) = 2.076923076923077d+00
+        cell_coefficients(2, 1) = -1.076923076923077d+00
+        boundary_coefficients(1, 2) = -9.230769230769229d-01 * dx
+        boundary_coefficients(2, 2) = 7.692307692307693d-02 * dx**2
+        cell_coefficients(1, 2) = 1.076923076923077d+00
+        cell_coefficients(2, 2) = -7.692307692307693d-02
+    else if (orders == '13') then
+        boundary_coefficients(1, 1) = -3.000000000000000d+00 * dx
+        boundary_coefficients(2, 1) = -1.125000000000000d+00 * dx**3
+        cell_coefficients(1, 1) = 0.000000000000000d+00
+        cell_coefficients(2, 1) = 1.000000000000000d+00
+        boundary_coefficients(1, 2) = -1.000000000000000d+00 * dx
+        boundary_coefficients(2, 2) = -4.166666666666666d-02 * dx**3
+        cell_coefficients(1, 2) = 1.000000000000000d+00
+        cell_coefficients(2, 2) = 0.000000000000000d+00
+    else if (orders == '23') then
+        boundary_coefficients(1, 1) = 3.000000000000000d+00 * dx**2
+        boundary_coefficients(2, 1) = 5.000000000000000d-01 * dx**3
+        cell_coefficients(1, 1) = 3.000000000000000d+00
+        cell_coefficients(2, 1) = -2.000000000000000d+00
+        boundary_coefficients(1, 2) = 1.000000000000000d+00 * dx**2
+        boundary_coefficients(2, 2) = 5.000000000000000d-01 * dx**3
+        cell_coefficients(1, 2) = 2.000000000000000d+00
+        cell_coefficients(2, 2) = -1.000000000000000d+00
+    end if
+    
+end subroutine get_lower_bc_coefficients    
+
+
+subroutine get_upper_bc_coefficients(orders, dx, boundary_coefficients, cell_coefficients)
+    character*2, intent(in) :: orders
+    double precision, intent(in) :: dx
+    double precision, dimension(2, 2) :: boundary_coefficients
+    double precision, dimension(2, 2) :: cell_coefficients
+    
+    if (orders == '01') then
+        boundary_coefficients(1, 1) = -8.888888888888884d-01
+        boundary_coefficients(2, 1) = 1.333333333333333d+00 * dx
+        cell_coefficients(1, 1) = -1.111111111111111d-01
+        cell_coefficients(2, 1) = 2.000000000000000d+00
+        boundary_coefficients(1, 2) = -2.400000000000000d+01
+        boundary_coefficients(2, 2) = 1.200000000000000d+01 * dx
+        cell_coefficients(1, 2) = -2.000000000000000d+00
+        cell_coefficients(2, 2) = 2.700000000000000d+01
+    else if (orders == '02') then
+        boundary_coefficients(1, 1) = 2.000000000000000d+00
+        boundary_coefficients(2, 1) = 2.500000000000000d-01 * dx**2
+        cell_coefficients(1, 1) = -0.000000000000000d+00
+        cell_coefficients(2, 1) = -1.000000000000000d+00
+        boundary_coefficients(1, 2) = 2.000000000000000d+00
+        boundary_coefficients(2, 2) = 2.250000000000000d+00 * dx**2
+        cell_coefficients(1, 2) = -1.000000000000000d+00
+        cell_coefficients(2, 2) = -0.000000000000000d+00
+    else if (orders == '03') then
+        boundary_coefficients(1, 1) = 2.666666666666666d+00
+        boundary_coefficients(2, 1) = 1.666666666666666d-01 * dx**3
+        cell_coefficients(1, 1) = 3.333333333333333d-01
+        cell_coefficients(2, 1) = -2.000000000000000d+00
+        boundary_coefficients(1, 2) = 7.999999999999998d+00
+        boundary_coefficients(2, 2) = 1.500000000000000d+00 * dx**3
+        cell_coefficients(1, 2) = 2.000000000000000d+00
+        cell_coefficients(2, 2) = -8.999999999999998d+00
+    else if (orders == '12') then
+        boundary_coefficients(1, 1) = 9.230769230769229d-01 * dx
+        boundary_coefficients(2, 1) = 7.692307692307693d-02 * dx**2
+        cell_coefficients(1, 1) = -7.692307692307691d-02
+        cell_coefficients(2, 1) = 1.076923076923077d+00
+        boundary_coefficients(1, 2) = 9.230769230769229d-01 * dx
+        boundary_coefficients(2, 2) = 2.076923076923077d+00 * dx**2
+        cell_coefficients(1, 2) = -1.076923076923077d+00
+        cell_coefficients(2, 2) = 2.076923076923077d+00
+    else if (orders == '13') then
+        boundary_coefficients(1, 1) = 1.000000000000000d+00 * dx
+        boundary_coefficients(2, 1) = 4.166666666666666d-02 * dx**3
+        cell_coefficients(1, 1) = 0.000000000000000d+00
+        cell_coefficients(2, 1) = 1.000000000000000d+00
+        boundary_coefficients(1, 2) = 3.000000000000000d+00 * dx
+        boundary_coefficients(2, 2) = 1.125000000000000d+00 * dx**3
+        cell_coefficients(1, 2) = 1.000000000000000d+00
+        cell_coefficients(2, 2) = 0.000000000000000d+00
+    else if (orders == '23') then
+        boundary_coefficients(1, 1) = 9.999999999999998d-01 * dx**2
+        boundary_coefficients(2, 1) = -4.999999999999998d-01 * dx**3
+        cell_coefficients(1, 1) = -9.999999999999998d-01
+        cell_coefficients(2, 1) = 2.000000000000000d+00
+        boundary_coefficients(1, 2) = 3.000000000000000d+00 * dx**2
+        boundary_coefficients(2, 2) = -5.000000000000000d-01 * dx**3
+        cell_coefficients(1, 2) = -2.000000000000000d+00
+        cell_coefficients(2, 2) = 3.000000000000000d+00
+    end if
+
+end subroutine get_upper_bc_coefficients    
---
+++ lib/python/stencil_calculations.py
@@ -0,0 +1,88 @@
+from numpy import *
+
+def GetBoundaryCoefficients(derivative_orders,
+                            input_coordinates,
+                            output_coordinates):
+  def _DxSuffix(order):
+    if order == 0:
+      return ''
+    elif order == 1:
+      return ' * dx'
+    else:
+      return ' * dx**%d' % order
+      
+  num_conditions = len(derivative_orders) + len(input_coordinates)
+  rows = []
+  orders_to_row_indices = {}
+  for order in sorted(derivative_orders):
+    row = zeros(num_conditions)
+    row[order] = math.factorial(order)
+    orders_to_row_indices[order] = len(rows)
+    rows.append(row)
+
+  coordinates_to_row_indices = {}
+  for coordinate in input_coordinates:
+    coordinates_to_row_indices[coordinate] = len(rows)
+    rows.append([coordinate**i for i in xrange(num_conditions)])
+  
+  A = array(rows).T
+
+  lines = []
+
+  for i_output, coordinate in enumerate(output_coordinates):
+    coefficients = linalg.solve(A, [coordinate**i for i in xrange(num_conditions)])
+
+    for i, o in enumerate(derivative_orders):
+      coefficient = coefficients[orders_to_row_indices[o]]
+      coeff_string = ('%.15e' % coefficient).replace('e', 'd')
+      lines.append('boundary_coefficients(%d, %d) = %s%s' % 
+                   (i+1, i_output+1, coeff_string, _DxSuffix(o)))
+
+    for i, c in enumerate(input_coordinates):
+      coefficient = coefficients[coordinates_to_row_indices[c]]
+      coeff_string = ('%.15e' % coefficient).replace('e', 'd')
+      lines.append('cell_coefficients(%d, %d) = %s' % (i+1, i_output+1, coeff_string)) 
+
+  return lines
+
+
+if __name__ == '__main__':
+  cases = [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)]
+  input_coordinates = (.5, 1.5)
+  output_coordinates = (-.5, -1.5)
+
+  print 'Lower coefficients'
+  print '=================='
+  first = True
+  for case in cases:
+    if first:
+      print 'if (orders == \'%d%d\') then' % case
+      first = False
+    else:
+      print 'else if (orders == \'%d%d\') then' % case
+      
+    lines = GetBoundaryCoefficients(case, input_coordinates, output_coordinates)
+    lines = ['    ' + line for line in lines]
+    print '\n'.join(lines)
+  print 'end if'
+  
+  print '\n'
+  
+  print 'Upper coefficients'
+  print '=================='
+  input_coordinates = (-1.5, -.5)
+  output_coordinates = (.5, 1.5)
+  
+  first = True
+  for case in cases:
+    if first:
+      print 'if (orders == \'%d%d\') then' % case
+      first = False
+    else:
+      print 'else if (orders == \'%d%d\') then' % case
+      
+    lines = GetBoundaryCoefficients(case, input_coordinates, output_coordinates)
+    lines = ['    ' + line for line in lines]
+    print '\n'.join(lines)
+  print 'end if'
+  
