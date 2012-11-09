bf73abf87e735d0909baa92445815a657d24b067
Jonathan Claridge <claridge@gmail.com>
2012-11-08 19:14:12 -0800

    Fix Order4Nonlinear for speed.

--- applications/2d/Order4Nonlinear/apply_pde_operator.f90
+++ applications/2d/Order4Nonlinear/apply_pde_operator.f90
@@ -18,12 +18,27 @@ subroutine apply_pde_operator(t, q, output)
     double precision, dimension(1-mbc:mx+mbc, 1-mbc:my+mbc, meqn), intent(in) :: q
     double precision, dimension(1-mbc:mx+mbc, 1-mbc:my+mbc, meqn), intent(out) :: output
 
-    double precision, dimension(1-mbc:mx+mbc, 1-mbc:my+mbc) :: q_laplacian
+    double precision, dimension(1-mbc:mx+mbc, 1-mbc:my+mbc) :: q_laplacian, x_flux, y_flux
     integer :: ix, iy
     double precision, external :: derivative0
+    double precision :: q_face
 
 
     call calculate_laplacian(q(:,:,1), q_laplacian)
+
+    do iy = 1, my
+        do ix = 1, mx+1
+            q_face = derivative0(q(ix-2:ix+1, iy, 1))
+            x_flux(ix, iy) = q_face * (q_laplacian(ix, iy) - q_laplacian(ix-1, iy)) / dx
+        end do
+    end do
+
+    do iy = 1, my+1
+        do ix = 1, mx
+            q_face = derivative0(q(ix, iy-2:iy+1, 1))
+            y_flux(ix, iy) = q_face * (q_laplacian(ix, iy) - q_laplacian(ix, iy-1)) / dy
+        end do
+    end do    
 
     do iy = 1, my
         do ix = 1, mx
@@ -31,23 +46,6 @@ subroutine apply_pde_operator(t, q, output)
                 - (y_flux(ix, iy+1) - y_flux(ix, iy)) / dy
         end do
     end do
-    
-    
-    contains
-    
-    double precision function x_flux(ix, iy)
-        integer :: ix, iy
-        double precision :: q_face
-        q_face = derivative0(q(ix-2:ix+1, iy, 1))
-        x_flux = q_face * (q_laplacian(ix, iy) - q_laplacian(ix-1, iy)) / dx
-    end function x_flux
-
-    double precision function y_flux(ix, iy)
-        integer :: ix, iy
-        double precision :: q_face
-        q_face = derivative0(q(ix, iy-2:iy+1, 1))
-        y_flux = q_face * (q_laplacian(ix, iy) - q_laplacian(ix, iy-1)) / dy
-    end function y_flux
     
 end subroutine apply_pde_operator
 
